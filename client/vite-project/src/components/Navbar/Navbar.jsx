84c4dbfcd69a8580fab2d62d230005b6f3c68c21
Ravi Pajiyar <pajiyargravi20011@gmail.com>
2024-09-19 06:13:46 +0545

    minor fixes

--- client/vite-project/src/components/Navbar/Navbar.jsx
+++ client/vite-project/src/components/Navbar/Navbar.jsx
@@ -1,41 +1,46 @@
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom'; // Import useNavigate
-import Login from '../Login/Login'; // Make sure to import the Login component
+import { useNavigate } from 'react-router-dom';
+import Login from '../Login/Login';
 
 const Navbar = () => {
   const [showLogin, setShowLogin] = useState(false);
-  const [login, setLogin] = useState(false);
-  const navigate = useNavigate(); // Initialize useNavigate
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [username, setUsername] = useState('');
+  const navigate = useNavigate();
+
+  const handleLogin = (loggedInUsername) => {
+    setIsLoggedIn(true);
+    setUsername(loggedInUsername);
+    setShowLogin(false);
+  };
 
   return (
     <nav className="bg-white shadow-md p-4">
       <div className="container mx-auto flex justify-between items-center">
         <div className="flex items-center space-x-2">
-          {/* Make the text clickable */}
           <span
             className="text-xl font-bold text-black cursor-pointer"
-            onClick={() => navigate('/')} // Navigate to LandingPage on click
+            onClick={() => navigate('/')}
           >
             CourseCraft
           </span>
         </div>
-
         <div>
-          {!login ? (
+          {!isLoggedIn ? (
             <button
-              className={
-                'bg-purple-700 text-white py-2 px-4 rounded hover:bg-purple-600 transition duration-300'
-              }
+              className="bg-purple-700 text-white py-2 px-4 rounded hover:bg-purple-600 transition duration-300"
               onClick={() => setShowLogin(true)}
             >
               Login
             </button>
           ) : (
-            <p className=" text-black">Hi Sushankhya</p>
+            <p className="text-black">Hi {username}</p>
           )}
         </div>
       </div>
-      {showLogin && <Login close={() => setShowLogin(false)} />}
+      {showLogin && (
+        <Login close={() => setShowLogin(false)} onLogin={handleLogin} />
+      )}
     </nav>
   );
 };
